a1e3e79da75865e27e6729a2e3def5197ff4de6f
jacob <jacoblacsamana0@gmail.com>
2021-02-10 13:54:16 -0800

    implement Main menu interface

--- src/main/ui/PatriceApplication.java
+++ src/main/ui/PatriceApplication.java
@@ -8,14 +8,16 @@ public class PatriceApplication {
     //based off
     //https://github.students.cs.ubc.ca/CPSC210/TellerApp/blob/master/src/main/ca/ubc/cpsc210/bank/ui/TellerApp.java
     private ArrayList<PatriceWorkspace> openWorkspaces;
+    private ArrayList<String> patriceWorkspaceNames;
     private PatriceWorkspace activeWorkSpace;
     private Scanner userInput;
-    //TODO: multiple open sessions is not implemented yet, please disregard openWorkSpaces for now
 
     //EFFECT: create a new instance of the PATRICE application, and runs its execution loop;
     // creates a new list of PATRICE workspaces, and sets the current active workspace to null
+    // creates a list to hold all of the names of open workspaces
     public PatriceApplication() {
         openWorkspaces = new ArrayList<>();
+        patriceWorkspaceNames = new ArrayList<>();
         activeWorkSpace = null;
         runPatriceMenu();
     }
@@ -34,7 +36,9 @@ public class PatriceApplication {
                 displayApplicationUsageInfo();
                 System.out.println();
             } else if (chosenCmd.equals("new")) {
-                activeWorkSpace = new PatriceWorkspace();
+                createNewWorkspace();
+            } else if (chosenCmd.equals("switch")) {
+                switchWorkspaces();
             } else if (chosenCmd.equals("load")) {
                 //TODO: change this text when data persistence is implemented
                 System.out.println("Not yet implemented, please choose another action");
@@ -59,22 +63,28 @@ public class PatriceApplication {
                 + "00           00       00        00        00    00           00       00             00          \n"
                 + "00           00       00        00        00       00  00 00 00 00 00  00 00 00 00   00 00 00 00 \n"
                 + "================================================================================================ \n"
-                + "A Logical Expression-Circuit Translation App \n"
+                + "||                        A Logical Expression-Circuit Translation App                        || \n"
         );
     }
 
     //EFFECTS: Displays Main Menu commands
     private void displayMenuCommOptions() {
+        System.out.println(
+                "================================================================================================");
         System.out.println("Usage Information: info");
-        System.out.println("Open a new workspace: new");
+        System.out.println("Create a new workspace: new");
+        System.out.println("Switch to an already loaded workspace: switch [WIP]");
         System.out.println("Load an existing workspace load [WIP]");
         System.out.println("Close Program: x");
+        System.out.println(
+                "================================================================================================");
     }
 
     //EFFECTS: displays usage rules/info
     private void displayApplicationUsageInfo() {
         System.out.println(
-                  "==============================[Expression Usage Rules]==============================\n"
+                  "================================================================================================ \n"
+                + "====================================[Expression Usage Rules]==================================== \n"
                 + "-Can not be sequential\n"
                 + "-Must be well-formed\n"
                 + "-Currently only ~, ∨, and ∧ are supported currently\n"
@@ -83,10 +93,66 @@ public class PatriceApplication {
                 + " i.e '(~A ∧ B) ∨ A' is won't work, but '((~A ∧ B) ∨ A)' will\n"
                 + "-Please only use the available variable names and always in upper case\n"
                 + "\n"
-                + "==============================[Circuit Usage Rules]==============================\n"
+                + "=====================================[Circuit Usage Rules]====================================== \n"
                 + "-Can't be sequential \n"
                 + "-Must be well-formed \n"
+                + "\n"
+                + "================================================================================================"
         );
+    }
+
+    //EFFECTS: opens the given workspace by name if one can be found among the loaded workspaces
+    public void openLoadedWorkSpace(String workspaceName) {
+        for (PatriceWorkspace p: openWorkspaces) {
+            if (p.getWorkspaceName().equals(workspaceName)) {
+                System.out.println(p.getWorkspaceName() + " successfully loaded");
+                p.interactionLoop();
+                return;
+            }
+        }
+        System.out.println("specified workspace cannot be found");
+        return;
+    }
+
+    //EFFECTS: display names of all open workspaces
+    public String displayOpenworkSpaceNames() {
+        ArrayList<String> workspaceNames = new ArrayList<>();
+        for (PatriceWorkspace workSpace: openWorkspaces) {
+            workspaceNames.add(workSpace.getWorkspaceName());
+        }
+
+        return workspaceNames.toString();
+    }
+
+
+    //EFFECTS: creates a new workspace and prompts user for a name to call it
+    //if name is left blank, or user specifies a name already in use, will propmt them again until a valid
+    //name is provided
+    private void createNewWorkspace() {
+        String newWorkspaceName = "";
+        while (true) {
+            System.out.println("What do you want to call this workspace?");
+            userInput = new Scanner(System.in);
+            newWorkspaceName = userInput.next();
+
+            if (!patriceWorkspaceNames.contains(newWorkspaceName) || newWorkspaceName.equals("")) {
+                break;
+            }
+            System.out.println("Specified Name is in use or invalid.");
+        }
+        openWorkspaces.add(new PatriceWorkspace(newWorkspaceName));
+        patriceWorkspaceNames.add(newWorkspaceName);
+
+    }
+
+    //EFFECTS: prompts user for a name of an opened workspace, and opens it, if there is a
+    //workspace of that name in the list of workspaces
+    private void switchWorkspaces() {
+        displayOpenworkSpaceNames();
+        System.out.println("Name workspace to open");
+        userInput = new Scanner(System.in);
+        String wrkspaceToLoad = userInput.next();
+        openLoadedWorkSpace(wrkspaceToLoad);
     }
 
 
--- src/main/ui/PatriceWorkspace.java
+++ src/main/ui/PatriceWorkspace.java
@@ -2,6 +2,7 @@ package ui;
 
 import model.LogicalCircuit;
 import model.LogicalExpression;
+import java.util.Scanner;
 
 //an interactive workspace where the user can manipulate expressions and circuits
 // and convert them to one another
@@ -10,13 +11,42 @@ import model.LogicalExpression;
 public class PatriceWorkspace {
     LogicalCircuit localCircuit;
     LogicalExpression localExpression;
+    private Scanner userInput;
+    public final String workSpaceName;
 
-    //EFFECTS: creates a new workspace with an empty logical expression and a blank logical
-    //expression
-    public PatriceWorkspace() {
+
+    //EFFECTS: creates a new workspace with an empty logical expression and a blank logical, and sets workSpaceName
+    //expression.
+    // also starts the interaction loop
+    public PatriceWorkspace(String workSpaceName) {
         localCircuit = new LogicalCircuit();
         localExpression = new LogicalExpression();
+        this.workSpaceName = workSpaceName;
+        interactionLoop();
+
     }
+
+    //MODIFIES: localCircuit, localExpression
+    //EFFECT: starts the interactive loop that allows the user to interface with this workspace
+    public void interactionLoop() {
+        System.out.println(
+                "================================================================================================");
+        System.out.println("Current workspace: " + this.workSpaceName + "\n \n");
+
+
+    }
+
+    //EFFECT: display workspace interface command options
+    public void displayWorkspaceOptions() {
+
+    }
+
+    //EFFECT: returns workSpaceName
+    public String getWorkspaceName() {
+        return workSpaceName;
+    }
+
+    
 
 
 }
