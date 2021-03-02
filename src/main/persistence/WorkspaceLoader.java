0678c1cc1859e726946ffb3c027a5de10d291a2c
jacob <jacoblacsamana0@gmail.com>
2021-03-02 15:43:12 -0800

    add WorkspaceLoader and WorkspaceSaver classes to handle data persistence

---
+++ src/main/persistence/WorkspaceLoader.java
@@ -0,0 +1,6 @@
+package persistence;
+
+//represents a system that loads a PATRICE workstation from a given save file (in json format)
+public class WorkspaceLoader {
+
+}
---
+++ src/main/persistence/WorkspaceSaver.java
@@ -0,0 +1,6 @@
+package persistence;
+
+//represents a system that saves a PATRICE workspace to the local file system as a json file
+public class WorkspaceSaver {
+
+}
