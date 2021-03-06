995a301b2a6cbd66f8ea8c60b12facf1689df6e5
jacob <jacoblacsamana0@gmail.com>
2021-03-06 13:54:24 -0800

    added test classes for persistence class

--- src/test/model/LogicalCircuitTest.java
+++ src/test/model/LogicalCircuitTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.BeforeEach;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class LogicalCircuitTest {
---
+++ src/test/persistence/WorkspaceSaverTest.java
@@ -0,0 +1,23 @@
+package persistence;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import ui.PatriceApplication;
+import ui.PatriceWorkspace;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WorkspaceSaverTest {
+    PatriceApplication testPatriceApp;
+
+    @BeforeEach
+    public void setup(){
+        testPatriceApp = new PatriceApplication();
+
+
+    }
+
+
+
+
+}
